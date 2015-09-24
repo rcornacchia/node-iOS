6e6a742a6c6b9e7e620deccb2d037e073864e7d7
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-09-24 03:58:04 -0400

    Added express and installed into index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-// import http library into http variable
-var http = require('http');
+// import libraries
+var http = require('http'),
+    express = require('express');
+
+// Initialize Express app and set its port to 300 default
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+// Routing for / root
+app.get('/', function ( req, res) {
+    // send formats various response headers for you, like content-type and status code
+    res.send('<html><body><h1>Hello World</h1></body></html>');
+});
 
 // Creates server that responds to simple http requests
 // Responds with 200 response and page content
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<html><body><h1>Hello World</h1></body></html>');
-}).listen(3000);
-
-console.log('Server running on port 3000');
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
